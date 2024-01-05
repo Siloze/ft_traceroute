f48cbabc619f3f9dac4b732f65beb8a0e22dda1c
Siloze <samuel.janssens13@gmail.com>
2024-01-05 13:20:08 +0100

    oe

--- src/packet/udp.c
+++ src/packet/udp.c
@@ -1,10 +1,10 @@
 #include "../includes/traceroute.h"
 
 ssize_t sendUdpPacket(int sockfd, struct sockaddr *ai_addr, size_t packet_size){
-    ssize_t send = 0;
     char packet[packet_size];
 
-    send = sendto(sockfd, packet, packet_size, 0, ai_addr, sizeof(*ai_addr));
+    ft_memset(packet, 0, packet_size);
+    ssize_t send = sendto(sockfd, packet, packet_size, 0, ai_addr, sizeof(*ai_addr));
     if (send < 0 )
         perror("sendto");
     return send;
